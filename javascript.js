cf6866387c957bbda692d20f9839553dd2e71217
supertastisch123 <larstroester@t-online.de>
2022-08-18 21:04:21 +0200

    Button hinzugefügt funktion gegeben

--- javascript.js
+++ javascript.js
@@ -1,8 +1,28 @@
 /*ideen:
-git
+- git
 - hinzufügen
 - entfernen -->einfach das child davor?
 - Text bearbeiten
 - checkbox darkmode
 - responsiv mal testen?ww
 */
+
+var hinzu = document.getElementById("hinzu");
+var input = document.getElementById("input");
+var liste = document.getElementById("liste");
+var entf = document.getElementById("entf");
+
+
+
+
+hinzu.addEventListener('click', function() { hinzufuegen();});
+
+
+
+
+function hinzufuegen() {
+
+
+
+
+}
