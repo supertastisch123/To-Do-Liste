b0a43186631c06f6601f248bd555a2b62ce09af0
supertastisch123 <larstroester@t-online.de>
2022-09-10 22:39:51 +0200

    Fuktion mit enter hinzugefügt

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 /*ideen:
 - hinzufügen            OK
-- entfernen             
-- enter                 
+- entfernen             OK
+- enter                 OK
 - Text bearbeiten       
-- checkbox darkmode     
+  mit doppelclick?       
+- checkbox darkmode
+- Datenbank?
 
 
 - git
@@ -12,58 +14,89 @@
 
 var hinzu = document.getElementById("hinzu");
 var liste = document.getElementById("liste");
+var input = document.getElementById("input");
 
+
+hinzu.addEventListener('click', function () { hinzufuegen() });
+input.addEventListener("keypress", function(e){ enter(e)});
+
+var laenge= getLength();
 
 var entf = document.getElementById("entf");
+ 
+entf.addEventListener('click', function (e) { entfernen(e) });
 
 
 
 
-hinzu.addEventListener('click', function () {hinzufuegen()});
-//entf.addEventListener('click', function () { entfernen() });
 
 
 function hinzufuegen() {
 
+    var input = getInput();
 
-    if (getInput() != "") {
+    if (input != "") {
 
-        
         var punkt = document.createElement('li');
         var button = document.createElement('button')
-       
-            var zahl = document.getElementById("liste").children.length;
-            liste.appendChild(punkt);
-            console.log(zahl);
-            if(zahl==0){
-                zahl+1;
-            }else{
-                zahl+1; //sollte auf zwei da delete element dazukommt
-            }
-            liste.children[zahl].innerHTML = input.value;
-            //liste.children[zahl].classList.add(zahl);
-            
-    
-            const w =liste.children[zahl];
-            console.log(w);
 
-        //  liste.appendChild(button); <button id="entf">entfernen</button>
+
+        //Punkt zur Liste hinzugefügt
+
+        var zahl = getLength();
+
+        liste.appendChild(punkt);
+        zahl + 1;
+        liste.children[zahl].innerHTML = input;
+        // Falls man eine Klasse braucht: liste.children[zahl].classList.add(zahl);
+
+
+        //Button "entfernen"
+
+        var entfLength = getLength();
+
+        liste.appendChild(button);
+        liste.children[entfLength].innerHTML = "entfernen";
+        liste.children[entfLength].setAttribute("id","entf");
+        liste.children[entfLength].setAttribute("onclick","entfernen(this)"); //???????????????
     }
 
 
 
 }
 
-function entfernen() {
 
-
+function entfernen(e) {
+    var ent= e;
+    var punkt= e.previousElementSibling;
+    liste.removeChild(ent);
+    liste.removeChild(punkt);
 
 }
+
 
 function getInput() {
 
     var input = document.getElementById("input").value;
     console.log(input);
 
-    return(input);
+    return (input);
+}
+
+
+function getLength() {
+
+    var zahl = document.getElementById("liste").children.length;
+    console.log(zahl);
+    return (zahl);
+}
+
+
+
+function enter(e){
+
+if (e.key== "Enter")
+    hinzufuegen();
+
+
 }
